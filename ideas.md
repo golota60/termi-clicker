8a4b2418accfe6e20f4bc85a1d1589fb1a539986
Szymon Wiszczuk <golota60@gmail.com>
2022-05-14 23:06:46 +0200

    init script minor changes

---
+++ ideas.md
@@ -0,0 +1,6 @@
+// todo - dont commit this lol - make this a roadmap or something and only then commit
+
+- save profiles and saves overall with serialization and stuff
+- load saves via args with process.argv
+- release process should be fleshed out prolly
+- suspend(ctrl + z )/unsuspend(fg)
--- src/index.ts
+++ src/index.ts
@@ -1,59 +1,40 @@
-console.log('app ready');
+import process from 'process';
+import readline from 'readline';
+import logUpdate from 'log-update';
+import { handleGracefulExit } from './helpers/input';
 
-// let stdin = process.stdin;
-// readline.emitKeypressEvents(stdin);
-// stdin.setRawMode(true);
-// stdin.resume();
+console.log('app ready');
+let stdin = process.stdin;
+readline.emitKeypressEvents(stdin);
+stdin.setRawMode(true);
+stdin.resume();
 // stdin.setEncoding('utf-8');
 
-// stdin.on('keypress', function (key, a) {
-//   const { sequence, name } = a;
-//   // ctrl-c ( end of text )
-//   const ctrlC = '\x03';
-//   const ctrlX = '\x18';
-//   const ctrlZ = '\x1A';
+stdin.on('keypress', function (key, a) {
+  const { sequence, name, ctrl, meta, shift } = a;
 
-//   if (sequence === ctrlC) {
-//     handleGracefulExit();
-//   }
+  const ctrlC = '\x03';
+  const ctrlX = '\x18';
+  const ctrlZ = '\x1A';
 
-//   console.log(key, a);
-//   if (key.ctrl) {
-//     process.exit();
-//   }
-//   // if (key && key.ctrl && key.name == "c") process.exit();
-//   // if (key === "\u0003") {
-//   //   process.exit();
-//   // }
-//   // write the key to stdout all normal like
-//   process.stdout.write(key);
-// });
+  if (sequence === ctrlC) {
+    handleGracefulExit();
+  }
 
-// // import readline from "readline";
+  console.log(key, a);
+});
 
-// // const rl = readline.createInterface({
-// //   input: process.stdin,
-// //   output: process.stdout,
-// // });
+const frames = ['-', '\\', '|', '/'];
+let index = 0;
 
-// const frames = ['-', '\\', '|', '/'];
-// let index = 0;
+setInterval(() => {
+  const frame = frames[(index = ++index % frames.length)];
 
-// // setInterval(() => {
-// //   const frame = frames[(index = ++index % frames.length)];
-
-// //   logUpdate(
-// //     `
-// //         ♥♥
-// //    ${frame} unicorns ${frame}
-// //         ♥♥
-// // `
-// //   );
-// // }, 16);
-
-// // const xd = exec("ls", (err, stdout, stderr) => {
-// //   console.log(stdout);
-// //   console.error(err);
-// // });
-
-// // console.log(xd)
+  logUpdate(
+    `
+        ♥♥
+   ${frame} unicorns ${frame}
+        ♥♥
+`
+  );
+}, 16);
